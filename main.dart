1e2acd0b56c3c69a9d1a3651e46b93a2a3ab7843
Hari Pranav <haripranav98@gmail.com>
2019-01-14 15:10:51 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,15 +4,50 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      
-      return MaterialApp(home: Scaffold(
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.greenAccent,
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('TOP 10 ANIME'),
+          title: Text('Top 10 anime'),
+        ),
+        backgroundColor: Colors.greenAccent,
+        body:  Center(child: Text('My Page!')),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Otaku Gozaimas'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Avicii'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Hardwell'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
       ),
-      ), 
-      );
-    }
+    );
+  }
 }
